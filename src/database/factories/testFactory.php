4e443381208629b8eff34259df4b7e491b36e05c
petri <pvcoetzee@gmail.com>
2023-04-06 00:05:39 +0200

    Seeing about modifying stubs and make commands to fit into the DDD model

---
+++ src/database/factories/testFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Lib\test;
+
+use App\Base\BaseFactory;
+
+/**
+ * @extends App\Base\BaseFactory
+ */
+class testFactory extends BaseFactory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
