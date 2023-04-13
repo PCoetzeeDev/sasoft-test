dd6d0287f34a23dd36b4b8552f4acd5ad9477be3
petri <pvcoetzee@gmail.com>
2023-04-13 10:17:01 +0200

    Getting started on the employee repo

--- src/app/Lib/Employee/EmployeeRepository.php
+++ src/app/Lib/Employee/EmployeeRepository.php
@@ -2,7 +2,20 @@
 
 namespace App\Lib\Employee;
 
+use Illuminate\Database\Eloquent\Collection;
+
 class EmployeeRepository
 {
-    // Something like getById() perhaps?
+    /**
+     * @param array $filterFields
+     * @return Collection
+     */
+    public static function searchWithFilter(array $filterFields = []) : Collection
+    {
+        $query = Employee::query()->get();
+        return $query;
+//        if (!empty($filterFields)) {
+//            $query->where()
+//        }
+    }
 }
