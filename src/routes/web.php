6214949c6e8580365d5f15b522331cecfcf292ab
petri <pvcoetzee@gmail.com>
2023-04-15 20:24:02 +0200

    Removing some laravel boiler plate

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,15 +32,3 @@ Route::name('employees')->prefix('/employees')->group(function () {
         ->middleware(\App\Http\Middleware\EmployeeCodeExists::class)
         ->name('.delete');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
